2e525714034bf03a0e61c125cf5d312196ce2433
subbhu7077 <suvratmajhi2027@gmail.com>
2026-09-04 22:04:19 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,15 @@
+// ============================================
+// MyMediaHub - Supabase Configuration
+// ============================================
 
+// 1. Apna Supabase Project URL yahan paste karo
+const SUPABASE_URL = "https://xrqtqdxbpebuysclzkki.supabase.co";
+
+// 2. Supabase Publishable Key yahan paste karni hai
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_HbhuZ1d0eeMi6ik7kp2LgQ_3EpN2Rv7";
+
+// Supabase client
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
